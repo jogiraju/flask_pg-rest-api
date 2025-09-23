650b6adb87e22f3bca72acec68162b25f79afd26
jogiraju <rajujogi.t@gmail.com>
2025-09-23 10:35:04 +0000

    /home ruute is added

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ def health():
     return jsonify({"status": "ok"}), 200
 
 # The below route is newly added
-@bp.route('/')
+@bp.route('/hello')
 def hello():
     return jsonify({"message": "Hello World!"}), 200
 
