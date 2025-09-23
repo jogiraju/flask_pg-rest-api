1345cff0f4b795b2b465a9d41aecac5e67be5dde
jogiraju <rajujogi.t@gmail.com>
2025-09-23 08:40:45 +0000

    app/routes.py changed

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,9 @@ bp = Blueprint('main', __name__)
 def health():
     return jsonify({"status": "ok"}), 200
 
+# The below route is newly added
 @bp.route('/')
 def hello():
     return jsonify({"message": "Hello World!"}), 200
+
+
